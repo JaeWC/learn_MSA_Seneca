16690f4943b6b23b82519f180f62eb697a855b4e
Jae Choi <j.thechois@gmail.com>
2019-04-07 00:51:17 +0900

    add action to seneca

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const seneca = require('seneca');
-const service = seneca();
+const service = seneca({ log: 'silent' });
 
-service.add({ math: 'sum' }, (msg, next) => {
-  next(null, {
-    sum: msg.values.reduce((total, value) => total + value, 0)
-  });
+const stack = [];
+
+// 값(value)이 필요하다는 것을 명시적으로 지정했으나, 값이 무엇인지는 신경 쓰지 않음
+service.add('stack:push,value:*', (msg, next) => {
+  stack.push(msg.value);
+
+  next(null, stack);
 });
 
-service.act({ math: 'sum', values: [1, 2, 3] }, (err, msg) => {
-  if (err) return console.error(err);
+service.add('stack:pop', (msg, next) => {
+  stack.pop();
 
-  console.log('sum = %s', msg.sum);
+  next(null, stack);
 });
+
+service.add('stack:get', (msg, next) => {
+  next(null, stack);
+});
+
+service.listen(3000);
