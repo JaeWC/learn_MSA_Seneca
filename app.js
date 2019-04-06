f1482dcbfb95db610077872a3c16023009c51f9d
Jae Choi <j.thechois@gmail.com>
2019-04-07 01:12:44 +0900

    entity plugin & jsonfile-store

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
+const async = require('async');
 const seneca = require('seneca');
-const service = seneca({ log: 'silent' });
+const service = seneca();
 
-const stack = [];
+service.use('basic');
+service.use('entity');
+service.use('jsonfile-store', { folder: 'data' });
 
-// 값(value)이 필요하다는 것을 명시적으로 지정했으나, 값이 무엇인지는 신경 쓰지 않음
-service.add('stack:push,value:*', (msg, next) => {
-  stack.push(msg.value);
+const stack = service.make$('stack');
 
-  next(null, stack);
+stack.load$(err => {
+  if (err) throw err;
+
+  service.add('stack:push,value:*', (msg, next) => {
+    stack.make$().save$({ value: msg.value }, err => {
+      return next(err, { value: msg.value });
+    });
+  });
+
+  service.add('stack:pop,value:*', (msg, next) => {
+    stack.list$({ value: msg.value }, (err, items) => {
+      async.each(
+        items,
+        (item, next) => {
+          item.remove$(next);
+        },
+        err => {
+          if (err) return next(err);
+          return next(err, { remove: items.length });
+        }
+      );
+    });
+  });
+
+  service.add('stack:get', (msg, next) => {
+    stack.list$((err, items) => {
+      if (err) return next(err);
+
+      return next(null, items.map(item => item.value));
+    });
+  });
+
+  service.listen(3000);
 });
-
-service.add('stack:pop', (msg, next) => {
-  stack.pop();
-
-  next(null, stack);
-});
-
-service.add('stack:get', (msg, next) => {
-  next(null, stack);
-});
-
-service.listen(3000);
